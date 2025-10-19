9cab8bb4f589118721d51f992e6f0fab0a1b624d
Gideon Osei Acheampong <gideonz823@gmail.com>
2025-10-19 14:02:24 -0700

    task 3

--- task_3.sql
+++ task_3.sql
@@ -1,7 +1,7 @@
 -- task_3.sql
 
--- List all tables in the database passed as argument
+-- List all tables in the database alx_book_store
 SELECT TABLE_NAME
 FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = DATABASE();
+WHERE TABLE_SCHEMA = 'alx_book_store';
 
