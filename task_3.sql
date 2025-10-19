e862a2f33f8094c5227983050822e054ba52d25b
Gideon Osei Acheampong <gideonz823@gmail.com>
2025-10-19 13:58:07 -0700

    task 3

--- task_3.sql
+++ task_3.sql
@@ -1,6 +1,6 @@
 -- task_3.sql
 
--- List all tables in the database that is currently selected (passed via mysql command)
+-- List all tables in the database passed as argument
 SELECT TABLE_NAME
 FROM INFORMATION_SCHEMA.TABLES
 WHERE TABLE_SCHEMA = DATABASE();
