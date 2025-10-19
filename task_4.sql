0b876aa2b9b6f84e779d0089d085e03030a0f93f
Gideon Osei Acheampong <gideonz823@gmail.com>
2025-10-19 13:47:42 -0700

    done

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,6 @@
 -- task_4.sql
 
--- List full description of the table 'books' in the currently selected database
+-- List full description of the table 'Books' in the database 'alx_book_store'
 SELECT 
     COLUMN_NAME,
     COLUMN_TYPE,
@@ -9,6 +9,6 @@ SELECT
     COLUMN_DEFAULT,
     EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE()
-  AND TABLE_NAME = 'books';
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
 
